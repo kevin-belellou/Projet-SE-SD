e7eeef282501a2e94ee70b764aeec771f637d2f5
Louis Schnellbach <xleek.okz@gmail.com>
2012-05-02 16:18:09 +0200

    Display ok

--- ConsoleAffichage.java
+++ ConsoleAffichage.java
@@ -17,10 +17,23 @@ public class ConsoleAffichage {
                     Thread.sleep(1000);
                     i++;
                     
-                    String[] resultat = c.getNoms();
+				//Renvoie un tableau de:
+				/*
+				* nom;
+				* temp;
+				* tempVoulue
+				* nivChauffage
+				* nivChauffageVoulu
+				*/
+                    String[] resultat = c.getInfos();
 
-                    for(int k = 0; k < resultat.length; k++)
-                         System.out.println(" le tableau monTableau = " + resultat[k]);
+                    for(int k = 0; k < resultat.length; k+=5) {
+                         System.out.println(" le tableau monTableau  = " + resultat[k]);
+                         System.out.println(" \t> Temperature        = " + resultat[k+1]);
+                         System.out.println(" \t> Temp. Voulue       = " + resultat[k+2]);
+                         System.out.println(" \t> Niv. Chauffage     = " + resultat[k+3]);
+                         System.out.println(" \t> Niv. Chauff Voulue = " + resultat[k+4]);
+				}
 
                     System.out.println("Demande " + i + " envoyee");
                }
